2a0df14fec623ffff185848f5acad8f737752d41
dnstudent <davide.nicoli1@studenti.unimi.it>
2022-07-09 12:06:17 +0200

    Commented and installed ariel_random

--- src/lib/ariel_random/ariel_random/ariel_random.cpp
+++ src/lib/ariel_random/ariel_random/ariel_random.cpp
@@ -17,66 +17,12 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 
 #define twom12 0.000244140625
 
-[[maybe_unused]] void ARandom ::SaveSeed() const {
-    std::ofstream WriteSeed;
-    WriteSeed.open("seed.out");
-    if (WriteSeed.is_open()) {
-        WriteSeed << m_l1 << " " << m_l2 << " " << m_l3 << " " << m_l4 << std::endl;
-    } else {
-        try {
-            WriteSeed.close();
-        } catch (...) {}
-        throw std::runtime_error("PROBLEM: Unable to open seed.out");
-    }
-    WriteSeed.close();
-}
-
-[[maybe_unused]] double ARandom ::Gauss(double mean, double sigma) {
-    double s = Rannyu();
-    double t = Rannyu();
-    double x = sqrt(-2 * log(1 - s)) * cos(2 * M_PI * t);
-    return mean + x * sigma;
-}
-
-double ARandom ::Rannyu(double min, double max) { return min + (max - min) * Rannyu(); }
-
-double ARandom ::Rannyu() {
-    int i1 = m_l1 * m_m4 + m_l2 * m_m3 + m_l3 * m_m2 + m_l4 * m_m1 + m_n1;
-    int i2 = m_l2 * m_m4 + m_l3 * m_m3 + m_l4 * m_m2 + m_n2;
-    int i3 = m_l3 * m_m4 + m_l4 * m_m3 + m_n3;
-    int i4 = m_l4 * m_m4 + m_n4;
-    m_l4 = i4 % 4096;
-    i3 = i3 + i4 / 4096;
-    m_l3 = i3 % 4096;
-    i2 = i2 + i3 / 4096;
-    m_l2 = i2 % 4096;
-    m_l1 = (i1 + i2 / 4096) % 4096;
-    return twom12 * (double(m_l1) +
-                     twom12 * (double(m_l2) + twom12 * (double(m_l3) + twom12 * double(m_l4))));
-}
-
-ARandom::result_type ARandom ::operator()() {
-    result_type i1 = m_l1 * m_m4 + m_l2 * m_m3 + m_l3 * m_m2 + m_l4 * m_m1 + m_n1;
-    result_type i2 = m_l2 * m_m4 + m_l3 * m_m3 + m_l4 * m_m2 + m_n2;
-    result_type i3 = m_l3 * m_m4 + m_l4 * m_m3 + m_n3;
-    result_type i4 = m_l4 * m_m4 + m_n4;
-    m_l4 = i4 % 4096UL;
-    i3 = i3 + i4 / 4096UL;
-    m_l3 = i3 % 4096UL;
-    i2 = i2 + i3 / 4096UL;
-    m_l2 = i2 % 4096UL;
-    m_l1 = (i1 + i2 / 4096UL) % 4096UL;
-    return b4096tob10<result_type>(m_l1, m_l2, m_l3, m_l4);
-}
-
-[[maybe_unused]] void ARandom ::SetRandom(result_type const *s, result_type p1, result_type p2) {
-    m_l1 = s[0];
-    m_l2 = s[1];
-    m_l3 = s[2];
-    m_l4 = s[3];
-    m_n3 = p1;
-    m_n4 = p2;
-}
+/**
+ * Initializes the number generator with the data in the provided files.
+ * @param seeds_source Path to a file containing the seed. Must be on a line starting with "RANDOMSEED" followed by four base-2^12 digits separated by spaces.
+ * @param primes_source Path to a file containing primes. Each line of the file must list the two base-2^12 digits representing a prime < 2^24, separated by a single space.
+ * @param primes_line The line of the primes file to use, starting from 1. If 0 use the default prime (11848219)
+ */
 ARandom::ARandom(const std::string_view &seeds_source, const std::string_view &primes_source,
                  size_t primes_line) {
     std::ifstream primes((std::string(primes_source)));
@@ -84,8 +30,8 @@ ARandom::ARandom(const std::string_view &seeds_source, const std::string_view &p
         result_type bucket;
         for (size_t i = 0; i < primes_line && !primes.eof(); i++) primes >> bucket >> bucket;
         if (!primes.eof()) {
-            primes >> m_n3 >> m_n4;
-            std::cout << "Prime " << b4096tob10<result_type>(0, 0, m_n3, m_n4) << " was used\n";
+            primes >> m_p3 >> m_p4;
+            std::cout << "Prime " << b4096tob10<result_type>(0, 0, m_p3, m_p4) << " was used\n";
         } else {
             throw std::runtime_error("The primes file was too short");
         }
@@ -112,16 +58,76 @@ ARandom::ARandom(const std::string_view &seeds_source, const std::string_view &p
         throw std::runtime_error("Could not open " + std::string(seeds_source));
     }
 }
-void ARandom::seed() {
-    m_n1 = 0UL;
-    m_n2 = 0UL;
-    m_n3 = 2892UL;
-    m_n4 = 2587UL;
-    m_l1 = 0UL;
-    m_l2 = 0UL;
-    m_l3 = 0UL;
-    m_l4 = 1UL;
+
+/**
+ * Generates a pseudorandom unsigned integer in the interval [0, (2^48)-1]
+ * @return
+ */
+ARandom::result_type ARandom ::operator()() {
+    result_type i1 = m_l1 * m_m4 + m_l2 * m_m3 + m_l3 * m_m2 + m_l4 * m_m1 + m_p1;
+    result_type i2 = m_l2 * m_m4 + m_l3 * m_m3 + m_l4 * m_m2 + m_p2;
+    result_type i3 = m_l3 * m_m4 + m_l4 * m_m3 + m_p3;
+    result_type i4 = m_l4 * m_m4 + m_p4;
+    m_l4 = i4 % 4096UL;
+    i3 = i3 + i4 / 4096UL;
+    m_l3 = i3 % 4096UL;
+    i2 = i2 + i3 / 4096UL;
+    m_l2 = i2 % 4096UL;
+    m_l1 = (i1 + i2 / 4096UL) % 4096UL;
+    return b4096tob10<result_type>(m_l1, m_l2, m_l3, m_l4);
 }
+
+
+[[maybe_unused]] void ARandom ::SaveSeed() const {
+    std::ofstream WriteSeed;
+    WriteSeed.open("seed.out");
+    if (WriteSeed.is_open()) {
+        WriteSeed << m_l1 << " " << m_l2 << " " << m_l3 << " " << m_l4 << std::endl;
+    } else {
+        try {
+            WriteSeed.close();
+        } catch (...) {}
+        throw std::runtime_error("PROBLEM: Unable to open seed.out");
+    }
+    WriteSeed.close();
+}
+
+#ifdef __apple_build_version__
+[[maybe_unused]] double ARandom ::Gauss(double mean, double sigma) {
+    double s = Rannyu();
+    double t = Rannyu();
+    double x = sqrt(-2 * log(1 - s)) * cos(2 * M_PI * t);
+    return mean + x * sigma;
+}
+
+double ARandom ::Rannyu(double min, double max) { return min + (max - min) * Rannyu(); }
+
+double ARandom ::Rannyu() {
+    int i1 = m_l1 * m_m4 + m_l2 * m_m3 + m_l3 * m_m2 + m_l4 * m_m1 + m_p1;
+    int i2 = m_l2 * m_m4 + m_l3 * m_m3 + m_l4 * m_m2 + m_p2;
+    int i3 = m_l3 * m_m4 + m_l4 * m_m3 + m_p3;
+    int i4 = m_l4 * m_m4 + m_p4;
+    m_l4 = i4 % 4096;
+    i3 = i3 + i4 / 4096;
+    m_l3 = i3 % 4096;
+    i2 = i2 + i3 / 4096;
+    m_l2 = i2 % 4096;
+    m_l1 = (i1 + i2 / 4096) % 4096;
+    return twom12 * (double(m_l1) +
+                     twom12 * (double(m_l2) + twom12 * (double(m_l3) + twom12 * double(m_l4))));
+}
+#endif
+
+[[maybe_unused]] void ARandom ::SetRandom(result_type const *s, result_type p1, result_type p2) {
+    m_l1 = s[0];
+    m_l2 = s[1];
+    m_l3 = s[2];
+    m_l4 = s[3];
+    m_p3 = p1;
+    m_p4 = p2;
+}
+
+
 void ARandom::seed(ARandom::result_type s) { b10tob4096(s, &m_l1, &m_l2, &m_l3, &m_l4); }
 
 /****************************************************************
--- src/lib/ariel_random/ariel_random/ariel_random.hpp
+++ src/lib/ariel_random/ariel_random/ariel_random.hpp
@@ -15,14 +15,24 @@ _/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
 #include <limits>
 #include <string_view>
 
+/**
+ * Converts from base 2^12 to base 10
+ * @param a,b,c,d Base-2^12 digits of the number to be converted from the most to the least significant.
+ */
 template<typename uint>
-constexpr uint b4096tob10(uint a, uint b, uint c, uint d) {
+constexpr uint b4096tob10(uint a, uint b, uint c, uint d) noexcept {
     return d + static_cast<uint>(4096U) *
                        (c + static_cast<uint>(4096U) * (b + static_cast<uint>(4096U) * a));
 }
 
+/**
+ * Converts from base 10 to base 2^12.
+ * @param x The decimal number to be converted. Must be < 2^48.
+ * @param a, b, c, d Pointers to the base-2^12 digits from the most to the least significant.
+ */
 template<typename uint>
 constexpr void b10tob4096(uint x, uint *a, uint *b, uint *c, uint *d) {
+    if (x < 4096UL * 4096UL * 4096UL * 4096UL) { throw; }
     *d = x % 4096U;
     x /= 4096U;
     *c = x % 4096U;
@@ -32,60 +42,60 @@ constexpr void b10tob4096(uint x, uint *a, uint *b, uint *c, uint *d) {
     *a = x % 4096U;
 }
 
-// TODO: cleanup
+/**
+ * Class implementing the RANNYU routine, a 48-bit linear congruential generator with specific choices for the multiplicator and the prime addends.
+ * Computation is performed in base 2^12.
+ * The procedure was adapted from the provided file to fit with modern c++ algorithms and to be built in a cmake project.
+ * Global variables were replaced with a class managing the procedure state and URBG requirements were implemented.
+ */
 class ARandom {
 protected:
 public:
-    typedef uint64_t result_type;
+    typedef uint_fast64_t result_type;
     // constructors
     ARandom() = default;
-    ARandom(ARandom &x) = default;
-    [[maybe_unused]] explicit ARandom(result_type s) : ARandom() { seed(s); }
-    template<typename Sseq>
-    [[maybe_unused]] explicit ARandom(Sseq &q) : ARandom() {
-        seed(q);
-    }
+    explicit ARandom(result_type s) : ARandom() { seed(s); }
     ARandom(const std::string_view &, const std::string_view &, size_t = 1);
-    [[maybe_unused]] void seed();
-    void seed(result_type s);
-    template<typename Sseq>
-    void seed(Sseq & /*q*/) {}
-    [[maybe_unused]] void discard(unsigned long long z) {
-        for (auto i = 0ULL; i < z; i++) operator()();
-    }
-    inline bool operator==(const ARandom &oth) const {
-        return (m_l1 == oth.m_l1 && m_l2 == oth.m_l2 && m_l3 == oth.m_l3 && m_l4 == oth.m_l4 &&
-                m_m1 == oth.m_m1 && m_m2 == oth.m_m2 && m_m3 == oth.m_m3 && m_m4 == oth.m_m4 &&
-                m_n1 == oth.m_n1 && m_n2 == oth.m_n2 && m_n3 == oth.m_n3 && m_n4 == oth.m_n4);
-    }
-    inline bool operator!=(const ARandom &oth) const { return !(*this == oth); }
-    // Should be implemented but who cares
-    template<typename os>
-    friend os &operator<<(os & /*o*/, const ARandom & /*rng*/) {}
-    template<typename is>
-    friend is &operator>>(is & /*i*/, ARandom & /*rng*/) {}
-    // methods
-    [[maybe_unused]] void SetRandom(result_type const *, result_type, result_type);
-    [[maybe_unused]] void SaveSeed() const;
-    [[maybe_unused]] double Rannyu();
+
+    /*------------------
+     * URBG methods: they enable ARandom to be used with the stanadrd library <random> distributions
+     ------------------*/
     result_type operator()();
     [[nodiscard]] [[maybe_unused]] static constexpr result_type min() { return 0UL; }
     [[nodiscard]] [[maybe_unused]] static constexpr result_type max() {
         // 2^48 - 1
         return 281474976710655UL;
     }
+
+    /**
+     * Seeds the generator with a base 10 number
+     * @param s Seed in base 10
+     */
+    [[maybe_unused]] void seed(result_type s);
+
+    /**
+     * Sets the seed and the two least significant base-2^12 digits of the prime (unsigned decimals < 4096)
+     * @param seed Seed in base 2^12 as four decimals
+     * @param prime3 Second least significant base-2^12 digit
+     * @param prime4 Least significant base-2^12 digit
+     */
+    void SetRandom(result_type const *seed, result_type prime3, result_type prime4);
+    void SaveSeed() const;
+#ifdef __apple_build_version__
+    [[maybe_unused]] double Rannyu();
     [[maybe_unused]] double Rannyu(double min, double max);
     [[maybe_unused]] double Gauss(double mean, double sigma);
+#endif
 
 private:
-    // moltiplicatore
+    // multiplyer in base 2^12
     const result_type m_m1{502UL}, m_m2{1521UL}, m_m3{4071UL}, m_m4{2107UL};
-    // prime due word del prime
-    result_type m_n1{0UL}, m_n2{0UL};
+    // most significant digits of the prime in base 2^12
+    const result_type m_p1{0UL}, m_p2{0UL};
     // seed
     result_type m_l1{0UL}, m_l2{0UL}, m_l3{0UL}, m_l4{1UL};
-    // seconde due word del prime
-    result_type m_n3{2892UL}, m_n4{2587UL};
+    // least significant digits of the prime
+    result_type m_p3{2892UL}, m_p4{2587UL};
 };
 
 #endif
